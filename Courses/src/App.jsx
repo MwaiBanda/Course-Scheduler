db3785b586aed1c1bfc17168f91d2331d788d3f0
Mwai Banda <bandamwai@gmail.com>
2023-04-24 18:53:18 -0500

    Added all required routes

--- Courses/src/App.jsx
+++ Courses/src/App.jsx
@@ -2,17 +2,44 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import CoursesPage from './pages/CoursesPage'
 import TeachersPage from './pages/TeachersPage';
+import SchedulePage from './pages/SchedulePage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import URL404 from './pages/404Page';
 import TopNavBar from './components/Navbar';
 
 function App() {
 
   return (
     <>
-    <TopNavBar/>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CoursesPage />}></Route>
-        <Route path="/teachers" element={<TeachersPage />}></Route>
+        <Route path="/" element={
+          <>
+           <TopNavBar/>
+           <CoursesPage />
+          </>
+        }></Route>
+        <Route path="*" element={<URL404 />}></Route>
+        <Route path="/cart" element={
+           <>
+           <TopNavBar/>
+           <CartPage />
+          </>
+        }></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/schedule" element={
+          <>
+          <TopNavBar/>
+          <SchedulePage />
+         </>
+        }></Route>
+        <Route path="/teachers" element={
+          <>
+          <TopNavBar/>
+          <TeachersPage />
+         </>
+        }></Route>
       </Routes>
     </BrowserRouter>
     </>
---
+++ Courses/src/pages/404Page.jsx
@@ -0,0 +1,3 @@
+export default function URL404() {
+    return <h1>404</h1>
+}
